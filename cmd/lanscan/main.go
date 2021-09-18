dc9c560ea771039a768e02a9ecd1ba7f723b4c2f
Sergey Zasenko <sergii@zasenko.name>
2021-09-18 17:26:21 +0300

    perform multiple scans for better result

--- cmd/lanscan/main.go
+++ cmd/lanscan/main.go
@@ -8,7 +8,11 @@ import (
 	"github.com/und3f/lanscan/scanner"
 )
 
+const SCAN_TIMES = 3
+
 func main() {
+	hosts := make(map[string]scanner.Host)
+
 	scanRange, err := scanner.ParseCIDR(os.Args[1])
 	if err != nil {
 		log.Fatal("Failed to parse scanning range %s", err)
@@ -16,11 +20,18 @@ func main() {
 	networkScanner := scanner.NewPingScanner()
 
 	networkScanner.SetHostFoundHandler(func(host scanner.Host) {
+		if _, exists := hosts[host.IP.String()]; exists {
+			return
+		}
+
+		hosts[host.IP.String()] = host
 		fmt.Println("Discovered", host.String())
 	})
 
 	fmt.Println("Discovering hosts...")
-	if err := networkScanner.Scan(scanRange); err != nil {
-		log.Fatalf("Failed to start scanning: %s", err)
+	for i := 0; i < SCAN_TIMES; i++ {
+		if err := networkScanner.Scan(scanRange); err != nil {
+			log.Fatalf("Failed to start scanning: %s", err)
+		}
 	}
 }
